88ca3efa52e765dde57dd95e72a90fd207941045
Clara-Faquinela <160731088+Clara-Faquinela@users.noreply.github.com>
2024-11-29 11:04:16 +0000

    final

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Charada',
+    'Quanto mais se perde, mais se tem?',
+    'O sono'
+)
+criaCartao(
+    'Charada',
+    'Eu sempre estou à sua frente, mas nunca poderei ser visto. O que sou?',
+    'O futuro'
+)
+
+criaCartao(
+    'Charada',
+    'O que sobe quando a chuva desce?',
+    'O guarda-chuva'
+)
+
+criaCartao(
+    'Charada',
+    'Quanto mais você tira de mim, maior eu fico. o que sou?',
+    'O buraco'
+)
+
+criaCartao(
+    'Charada',
+    'O que corre a sua volta mas nunca se move?',
+    'Uma cerca'
+)
